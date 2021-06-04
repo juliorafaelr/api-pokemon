5b7161e4aa54aa724c059cee0b14237d5d009af3
Julio Rafael Rivas Marcano <juliorafaelr@gmail.com>
2021-06-04 15:24:23 -0300

    final formatting

--- app/Http/Controllers/SentryController.php
+++ app/Http/Controllers/SentryController.php
@@ -37,11 +37,11 @@ class SentryController extends Controller
                     'fields' => [
                         [
                             'type' => 'mrkdwn',
-                            'text' => '*Project:*\n' . data_get($request, 'project')
+                            'text' => "*Project:*\n" . data_get($request, 'project')
                         ],
                         [
                             'type' => 'mrkdwn',
-                            'text' => '*Link:*\n<' . data_get($request, 'url') . '|sentry>'
+                            'text' => "*Link:*\n<" . data_get($request, 'url') . "|sentry>"
                         ]
                     ]
                 ],
@@ -50,7 +50,7 @@ class SentryController extends Controller
                     'fields' => [
                         [
                             'type' => 'mrkdwn',
-                            'text' => '*Environment:*\n' . data_get($request, 'event.environment')
+                            'text' => "*Environment:*\n" . data_get($request, 'event.environment')
                         ]
                     ]
                 ]
